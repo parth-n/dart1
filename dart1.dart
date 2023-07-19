329afb823843abd61c69428333dfe22673fd2297
Parth Nepalia <parth.nepalia@gmail.com>
2023-07-20 01:20:52 +0530

    string and interpolation

--- dart1.dart
+++ dart1.dart
@@ -8,7 +8,7 @@ void main() {
   // all the data types in dart are objects and their default value is null
   // it is not necessary to specify data tvype as it understands by the initial value
 
-  int age = 10;
+  /* int age = 10;
   //OR
   var age1 = 10;
   String name = "parth";
@@ -18,4 +18,17 @@ void main() {
   print(age1);
   print(name);
   print(name1);
+ */
+
+  // strings, interpolation
+
+  /* String name = "parth";
+  String message = "the name is $name"; //this is interpolation
+  print(message);
+  int i = name.length; // to get length of string
+  
+  print("the length of the string is " + name.length.toString());
+  // or
+  print("the length of the string is ${name.length} ");
+   */
 }
