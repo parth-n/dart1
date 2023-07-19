9a1c4dfa17c3825e9932651ffb324676e4bcae43
Parth Nepalia <parth.nepalia@gmail.com>
2023-07-20 00:09:22 +0530

    first commit

---
+++ dart1.dart
@@ -0,0 +1,21 @@
+void main() {
+  /* print("hello");
+  print("first dart program");
+  print(4);
+  print(true);
+ */
+  // data types are int ,double,boolean ,strings, runes,lists(arrays),maps
+  // all the data types in dart are objects and their default value is null
+  // it is not necessary to specify data tvype as it understands by the initial value
+
+  int age = 10;
+  //OR
+  var age1 = 10;
+  String name = "parth";
+  // or
+  var name1 = "parth";
+  print(age);
+  print(age1);
+  print(name);
+  print(name1);
+}
