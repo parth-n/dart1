137c640f3c80f29bb533fae44bfc44bc368de080
Parth Nepalia <parth.nepalia@gmail.com>
2023-07-28 15:10:51 +0530

    data types_1

--- dart1.dart
+++ dart1.dart
@@ -1,14 +1,14 @@
 void main() {
-  /* print("hello");
+  print("hello");
   print("first dart program");
   print(4);
   print(true);
- */
+
   // data types are int ,double,boolean ,strings, runes,lists(arrays),maps
   // all the data types in dart are objects and their default value is null
   // it is not necessary to specify data tvype as it understands by the initial value
 
-  /* int age = 10;
+  int age = 10;
   //OR
   var age1 = 10;
   String name = "parth";
@@ -18,17 +18,15 @@ void main() {
   print(age1);
   print(name);
   print(name1);
- */
 
   // strings, interpolation
 
-  /* String name = "parth";
+  String name = "parth";
   String message = "the name is $name"; //this is interpolation
   print(message);
   int i = name.length; // to get length of string
-  
+
   print("the length of the string is " + name.length.toString());
   // or
   print("the length of the string is ${name.length} ");
-   */
 }
