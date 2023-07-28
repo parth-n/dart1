fe5b8fcdb42069dd11d6317634b11ba4dec031f2
Parth Nepalia <parth.nepalia@gmail.com>
2023-07-28 15:20:17 +0530

    type conversion

--- data types.dart
+++ data types.dart
@@ -1,3 +1,19 @@
 void main() {
   // type conversion
+  //string to int
+  // int.parse()
+  String value = "60";
+
+  print(value.runtimeType);
+
+  int num = int.parse(value);
+  int total = 100 + num;
+
+  print(total);
+
+  // double to int , toInt() method
+  double num1 = 100.99;
+  int num2 = num1.toInt();
+
+  print(num2);
 }
