caf4202a954441d5df390756727eeb61ef745cd0
Parth Nepalia <parth.nepalia@gmail.com>
2023-07-29 01:52:07 +0530

    stdin

--- operators.dart
+++ operators.dart
@@ -3,7 +3,13 @@ import 'dart:io';
 void main() {
   print("enter your name : ");
 
-  String name = stdin.readLineSync()!;
+  //String? name = stdin.readLineSync(); //to input string
 
-  print("the name is $name");
+  //print("the name is $name");
+
+  print("enter the number: ");
+  String? str = stdin.readLineSync();
+  int? number = int.parse(str.toString());
+  int square = number * number;
+  print("the square is $square");
 }
