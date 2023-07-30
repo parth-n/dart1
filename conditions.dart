09e57f278774f852d35f69c937be3087c856b452
Parth Nepalia <parth.nepalia@gmail.com>
2023-07-30 15:55:08 +0530

    commit 1 for conditions

--- conditions.dart
+++ conditions.dart
@@ -1,1 +1,13 @@
-void main() {}
+void main() {
+// conditions
+// if , else, if else if , switch
+// condition based on boolean values
+
+  bool isMarried = false;
+
+  if (isMarried) {
+    print("you are married");
+  } else {
+    print("you are single");
+  }
+}
