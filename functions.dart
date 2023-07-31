893bf614d51ed4f9902041438cef46a088a372c9
Parth Nepalia <parth.nepalia@gmail.com>
2023-07-31 16:01:14 +0530

    functions and type commit

--- functions.dart
+++ functions.dart
@@ -1,1 +1,17 @@
-void main() {}
+void add(int n1, int n2) {
+  // n1 and n2 are parameters of the function
+  int sum = n1 + n2;
+  print("the sum is $sum");
+}
+
+void main() {
+  // type of func
+  /* no parameter and no return type
+  parameter and no return type
+  no parameter and return type
+  parameter and return type 
+*/
+
+// 10 and 30 are arguments
+  add(10, 30);
+}
