4275dce05cb1961c787b9aad88aeaa7faa96797a
Parth Nepalia <parth.nepalia@gmail.com>
2023-08-02 16:29:37 +0530

    commit 1 in list.dart

--- List.dart
+++ List.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 void main() {
   List<String> names = ["Ram", "John", "Mark", "Rocky"];
   List<int> integer = [12, 30, 23, 35];
@@ -16,4 +18,20 @@ void main() {
   // growable list
   var list3 = [200, 100, 210, 320];
   print(list3);
+
+  // mixed list
+  var list2 = ["mark", 22, "abhay", true];
+  print(list2);
+
+  // mutable list
+  List<String> mutable = ["zero", "one", "two", "three"];
+  print(mutable);
+  mutable[0] = "mark";
+  print("this is the mutable list $mutable");
+
+  // immutable list
+  const List<String> immutable = ["zero," "one", "two", "three"];
+  print(immutable);
+  // this followuing line will give exception handling
+  //immutable[0] = "mark";
 }
