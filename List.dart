e4982234d29cb1448c7768bc7a5478783fac9c19
Parth Nepalia <parth.nepalia@gmail.com>
2023-08-02 17:27:38 +0530

    commit 2

--- List.dart
+++ List.dart
@@ -32,6 +32,35 @@ void main() {
   // immutable list
   const List<String> immutable = ["zero," "one", "two", "three"];
   print(immutable);
-  // this followuing line will give exception handling
+  // this followuing line will give  Unhandled exception
   //immutable[0] = "mark";
+
+  List<String> drinks = ["water", "juice", "mocktail"];
+  print("the first element is ${drinks.first} ");
+  print("the last element is ${drinks.last} ");
+  print("${drinks.isEmpty.toString()}");
+  print("${drinks.isNotEmpty.toString()} ");
+  print("the reversed list is ${drinks.reversed} ");
+
+  // adding items
+  List<int> list4 = [2, 4, 6, 8, 10];
+  print(list4);
+  list4.add(12); // adds 12 at the end
+  print(list4);
+
+  list4.addAll([12, 14, 16, 18, 20]); //adds all elements at the end
+  print(list4);
+
+  // insert
+  list4.insert(5, 15); // inserted 15 at 5th index
+  print("after insert $list4");
+
+  list4.insertAll(9, [1, 2, 3, 4]);
+  print("after insert all $list4");
+
+  // replace range
+  List<int> list5 = [10, 15, 20, 25, 30];
+  print("this is before replace range $list5");
+  list5.replaceRange(0, 4, [5, 6, 7, 8]);
+  print("this is after replace range $list5");
 }
